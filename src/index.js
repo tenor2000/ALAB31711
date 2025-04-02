92c635ed38ac7139d70f72e9149cf7dfc62f1f91
tenor2000 <tenor2000@gmail.com>
2025-04-02 11:38:51 -0400

    Part 4 completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-"use strict";
 // @ts-check
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
@@ -63,4 +62,73 @@ var myBuick = new Car("Buick", "Regal");
 myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
 console.log(myBuick.model);
-//# sourceMappingURL=index.js.map
+// Part 3 Creating a Generic Class
+var NCycle = /** @class */ (function () {
+    function NCycle(make, model, wheels) {
+        this.make = make;
+        this.model = model;
+        this.wheels = wheels;
+        this.status = "started";
+        this.make = make;
+        this.model = model;
+        this.wheels = wheels;
+    }
+    NCycle.prototype.start = function () {
+        this.status = "started";
+    };
+    NCycle.prototype.stop = function () {
+        this.status = "stopped";
+    };
+    NCycle.prototype.print = function (index) {
+        if (index === void 0) { index = 0; }
+        if (!(this.make instanceof Array) && !(this.model instanceof Array)) {
+            console.log("This is a ".concat(this.make, " ").concat(this.model, " NCycle."));
+        }
+        else if (this.make instanceof Array && this.model instanceof Array) {
+            "This NCycle has a ".concat(this.make[index], " ").concat(this.model[index], " at ").concat(index, ".");
+        }
+        else {
+            console.log("This NCycle was not created properly.");
+        }
+    };
+    NCycle.prototype.printAll = function () {
+        if (this.make instanceof Array &&
+            this.model instanceof Array &&
+            this.model.length === this.make.length) {
+            for (var i = 0; i < this.make.length; i++) {
+                this.print(i);
+            }
+        }
+        else if (this.make instanceof String && this.model instanceof String) {
+            this.print();
+        }
+        else {
+            console.log("This NCycle was not created properly.");
+        }
+    };
+    return NCycle;
+}());
+// Part 4: Testing
+// Rudimentary testing Code, not part of the assignment
+var testCycle1 = new NCycle(1, 2, 3);
+testCycle1.print();
+testCycle1.printAll();
+var testCycle2 = new NCycle("This", "That", 4);
+testCycle2.print();
+testCycle2.printAll();
+// const testCycle3 = new NCycle<string>("Make", 10, 4);
+// testCycle3.print(4);
+// testCycle3.printAll();
+var makes4 = ["Volkswagon", "Tesla", "Audi"];
+var models4 = ["Passat", "Model X", "A4"];
+var testCycle4 = new NCycle(makes4, models4, 4);
+testCycle4.print(2);
+testCycle4.printAll();
+var makes5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var models5 = [1, 1, 2, 3, 5];
+var testCycle5 = new NCycle(makes5, models5, 0);
+testCycle5.print(7);
+testCycle5.printAll();
+// add(testCycle1.make, testCycle5.model[1]);
+// // Error expected here
+// add(testCycle2.make, testCycle4.model[1]);
--- src/index.ts
+++ src/index.ts
@@ -43,19 +43,23 @@ function printStatus(vehicle: Vehicle) {
 const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
 myHarley.start();
 printStatus(myHarley);
-console.log(myHarley.make.toUpperCase());
+// console.log(myHarley.make.toUpperCase());
 
 const myBuick = new Car("Buick", "Regal");
 myBuick.wheels = myBuick.wheels - 1;
-console.log(myBuick.wheels);
-console.log(myBuick.model);
+// console.log(myBuick.wheels);
+// console.log(myBuick.model);
 
 // Part 3 Creating a Generic Class
 
 class NCycle<T> {
   status: "started" | "stopped" = "started";
 
-  constructor(public make: T | T[], public model: T | T[], public wheels: T) {
+  constructor(
+    public make: T | T[],
+    public model: T | T[],
+    public wheels: number
+  ) {
     this.make = make;
     this.model = model;
     this.wheels = wheels;
@@ -70,18 +74,16 @@ class NCycle<T> {
     if (!(this.make instanceof Array) && !(this.model instanceof Array)) {
       console.log(`This is a ${this.make} ${this.model} NCycle.`);
     } else if (this.make instanceof Array && this.model instanceof Array) {
-      `This NCycle has a ${this.make[index]} ${this.model[index]} at ${index}.`;
+      console.log(
+        `This NCycle has a ${this.make[index]} ${this.model[index]} at ${index}.`
+      );
     } else {
       console.log("This NCycle was not created properly.");
     }
   }
 
   printAll(): void {
-    if (
-      this.make instanceof Array &&
-      this.model instanceof Array &&
-      this.model.length === this.make.length
-    ) {
+    if (this.make instanceof Array && this.model instanceof Array) {
       for (let i = 0; i < this.make.length; i++) {
         this.print(i);
       }
@@ -94,3 +96,31 @@ class NCycle<T> {
 }
 
 // Part 4: Testing
+// Rudimentary testing Code, not part of the assignment
+const testCycle1 = new NCycle<number>(1, 2, 3);
+testCycle1.print();
+testCycle1.printAll();
+
+const testCycle2 = new NCycle<string>("This", "That", 4);
+testCycle2.print();
+testCycle2.printAll();
+
+// const testCycle3 = new NCycle<string>("Make", 10, 4);
+// testCycle3.print(4);
+// testCycle3.printAll();
+
+const makes4 = ["Volkswagon", "Tesla", "Audi"];
+const models4 = ["Passat", "Model X", "A4"];
+const testCycle4 = new NCycle<string[]>(makes4, models4, 4);
+testCycle4.print(2);
+testCycle4.printAll();
+
+const makes5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const models5 = [1, 1, 2, 3, 5];
+const testCycle5 = new NCycle<number[]>(makes5, models5, 0);
+testCycle5.print(7);
+testCycle5.printAll();
+
+// add(testCycle1.make, testCycle5.model[1]);
+// // Error expected here
+// add(testCycle2.make, testCycle4.model[1]);
